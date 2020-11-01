b32a89c1a37ed857c2cb1683791ae4ef05921f10
WJ Yao <996267113@qq.com>
2020-11-01 22:38:21 +0800

    完成

--- plot_matrix.py
+++ plot_matrix.py
@@ -1,12 +1,21 @@
-# TODO: import ...
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+"""
+作业热身题，目的是熟悉github classroom的操作流程
+"""
 
 
 def generate_random_matrix(m, n):
-    raise NotImplementedError  # TODO: 删除该行，实现该函数功能
+    """生成一个m*n的二值矩阵，元素值为0,1"""
+    matrix = np.random.randint(0, 2, (m, n))
+    return matrix
 
 
 def save_matrix(matrix, file_name):
-    raise NotImplementedError  # TODO: 删除该行，实现该函数功能
+    """将矩阵保存为图片"""
+    plt.imsave(file_name, matrix, cmap='gray')
 
 
 if __name__ == "__main__":
